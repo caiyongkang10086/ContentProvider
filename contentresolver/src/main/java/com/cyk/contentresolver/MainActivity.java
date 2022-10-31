531db4bff394df2c61ab2c39234069a497cd3a32
caiyongkang <13169129293@163.com>
2022-11-01 01:01:47 +0800

    [bugfix/feature]practice how to read contacts
    Description:practice how to read contacts

--- contentresolver/src/main/java/com/cyk/contentresolver/MainActivity.java
+++ contentresolver/src/main/java/com/cyk/contentresolver/MainActivity.java
@@ -1,72 +1,123 @@
 package com.cyk.contentresolver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayAdapter adapter;
+    List<String> contactList = new ArrayList<>();
+
+    private Button add ;
+    private Button delete ;
+    private Button modified ;
+    private Button query ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Uri uri = Uri.parse("content://com.cyk.contentresolver.MainActivity/table1");
-        Button add = findViewById(R.id.add);
-        Button delete = findViewById(R.id.delete);
-        Button modified = findViewById(R.id.modified);
-        Button query = findViewById(R.id.query);
+        ListView contactsView = (ListView) findViewById(R.id.Contacts);
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactList);
+        contactsView.setAdapter(adapter);
+        add = findViewById(R.id.add);
+        delete = findViewById(R.id.delete);
+        modified = findViewById(R.id.modified);
+        query = findViewById(R.id.query);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ContentValues contentValues = new ContentValues();
-                contentValues.put("sentence", "i love chenguanyi");
-                contentValues.put("years", "999");
-                getContentResolver().insert(uri, contentValues);
             }
         });
 
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getContentResolver().delete(uri,"years = ?", new String[] { "1"});
             }
         });
 
         modified.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ContentValues contentValues = new ContentValues();
-                contentValues.put("sentence", "");
-                getContentResolver().update(uri, contentValues, "sentence = ?", new String[] {"i love chenguanyi"});
             }
         });
 
         query.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Cursor cursor = getContentResolver().query(
-                        uri,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null
-                );
-                while (cursor.moveToNext()) {
-                    @SuppressLint("Range")
-                    String sentence = cursor.getString(cursor.getColumnIndex("sentence"));
-                    @SuppressLint("Range")
-                    int years = cursor.getInt(cursor.getColumnIndex("years"));
-
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_CONTACTS}, 1);
+                } else {
+                    readContact();
                 }
             }
         });
+    }
+
+    private void readContact() {
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+            if(cursor != null) {
+                while (cursor.moveToNext()) {
+                    @SuppressLint("Range")
+                    String displayName = cursor.getString(cursor.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    @SuppressLint("Range")
+                    String number = cursor.getString(cursor.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contactList.add(displayName + "\n" + number);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    readContact();
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
     }
 }
